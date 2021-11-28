2238a6fdfc0428c0bddea694f61c1896c4951ffc
Parag <parag.raut@outlook.com>
2021-11-28 12:49:47 +0100

    little refactor

--- File_set.py
+++ File_set.py
@@ -34,7 +34,6 @@ def write_new_text(path_in):
     f.write(new_text)
     f.close()
     
-
 def open_file_in_append_mode(path_in):
     """To perform append operation"""
     f=open(path_in,'a')
@@ -95,53 +94,54 @@ def read_occurances(path_in):
     print 'The word "%s" is occured %d times in mentioned file'%(word_input,find_it.count(word_input))
     f.close()
 
-
-    
-
 if __name__=='__main__':
     print'------------------ Welcome To The File Handling Program ------------------ \n'
     print'################## Please Provide The Location Of File ################ \n'
-    path_input=raw_input('Paste file link here:->>>')
+    path_input=raw_input('Paste file link here:->>>').strip()
+    print'\n'
     show_input_path(path_input)
-    print'-----------------------------MENU-----------------------------'
-    print'''
-    1)Count the letters in text file
-    2)Count the words in the file
-    3)Tell the cursor position
-    4)Find the word if its their in file or not
-    5)Find the word and tell the cursor position
-    6)Add something to file
-    7)Delete the entire file and write new text
-    8)Read the whole file
-    9)Read file by line
-    10)To count the number of occurances of word in file
-    '''
+    while True:
+        print'\n-----------------------------MENU-----------------------------'
+        print'''
+        1) Count the letters in text file
+        2) Count the words in the file
+        3) Tell the cursor position
+        4) Find the word if its their in file or not
+        5) Find the word and tell the cursor position
+        6) Add something to file
+        7) Delete the entire file and write new text
+        8) Read the whole file
+        9) Read file by line
+        10) To count the number of occurances of word in file
+        11) Exit
+        '''
 
-    user_choice=int(raw_input('Enter your choice: '))
-    if user_choice==1:
-        count_length(path_input)
-    
-    elif user_choice==2:
-
-        count_words(path_input)
-    elif user_choice==3:
-        cursor_position(path_input)
-    elif user_choice==4:
-        find_word(path_input)
-    elif user_choice==5:
-        find_with_cur_pos(path_input)
-    elif user_choice==6:
-        open_file_in_append_mode(path_input)
-    elif user_choice==7:
-        delete_contents_of_file(path_input)
-        write_new_text(path_input)
-    elif user_choice==8:
-        open_file_in_read_mode(path_input)
-    elif user_choice==9:
-        read_by_line(path_input)
-    elif user_choice==10:
-        read_occurances(path_input)
-    else:
-        print'Please Provide Correct Input'
+        user_choice=int(raw_input('Enter your choice: '))
+        if user_choice==1:
+            count_length(path_input)    
+        elif user_choice==2:
+            count_words(path_input)
+        elif user_choice==3:
+            cursor_position(path_input)
+        elif user_choice==4:
+            find_word(path_input)
+        elif user_choice==5:
+            find_with_cur_pos(path_input)
+        elif user_choice==6:
+            open_file_in_append_mode(path_input)
+        elif user_choice==7:
+            delete_contents_of_file(path_input)
+            write_new_text(path_input)
+        elif user_choice==8:
+            open_file_in_read_mode(path_input)
+        elif user_choice==9:
+            read_by_line(path_input)
+        elif user_choice==10:
+            read_occurances(path_input)
+        elif user_choice==11:
+            print'Thank You For Using The Program'
+            break
+        else:
+            print'Please Provide Correct Input'
 print __doc__
 print'>>>By Parag Raut'
